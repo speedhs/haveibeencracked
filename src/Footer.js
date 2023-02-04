527d158cfd0b3098c8477534f1176dc16c7b1e89
speedhs <cocattackbased@gmail.com>
2023-02-04 16:21:42 +0530

    footer added

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 import './Footer.css';
 const FooterLinks = () => (
   <div>
-    <a href="#">Link 1</a>
+    <a href="https://darknetdiaries.com/episode/">Darknet Diaries</a>
     <a href="#">Link 2</a>
     <a href="#">Link 3</a>
   </div>
