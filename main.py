5ec7bf98f9c3e2439a8f48c11705554cf3745853
Fateh267 <84120241+Fateh267@users.noreply.github.com>
2023-03-11 00:07:42 +0530

    Connected Nav bar to all the pages. Updated main.py for the same. Added ir_model.py to lay the foundation for this project's IR model

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 
 app = Flask(__name__)
 
@@ -9,7 +9,19 @@ def App():
     if request.method == "POST":
         password = request.form.get("password")
         print(password)
-    return render_template('app.html')
+    return render_template('App.html')
+
+@app.route('/About')
+
+def About():
+
+    return render_template('About.html')
+
+@app.route('/Team')
+
+def Team():
+
+    return render_template('Team.html')
 
 
 
