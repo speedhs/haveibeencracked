4bc7c1a834251c6924536df8a574f551f44c2429
Fateh267 <84120241+Fateh267@users.noreply.github.com>
2023-03-18 14:32:33 +0530

    Added Jinja template for leaked password output

--- main.py
+++ main.py
@@ -7,12 +7,23 @@ app = Flask(__name__)
 
 def App():
     if request.method == "POST":
-        password = request.form.get("password")
-        print(password)
+        user_password = request.form.get("password")
+        data = user_password
+        return render_template('app.html', result = user_password)
+
     return render_template('app.html')
 
+@app.route('/about', methods =["GET", "POST"])
 
+def About():
 
+    return render_template('about.html')
+
+@app.route('/team', methods =["GET", "POST"])
+
+def Team():
+
+    return render_template('team.html')
 
 
 if __name__ == '__main__':
