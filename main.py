ae10fdc44b2c28548755170032e01b5f467420f8
speedhs <cocattackbased@gmail.com>
2023-03-19 16:10:24 +0530

    flask env changes

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, Response
 
 app = Flask(__name__)
 
@@ -28,5 +28,34 @@ def Team():
 
 def transform(p):
     return p+"pad"
+
+@app.route('/f.js' , methods = ["GET"])
+def f():
+    js_code = '''
+        function addDiv() {
+            const div = document.createElement("div");
+            const text = " hi there";
+            const divContent = document.createTextNode(text);
+            div.appendChild(divContent)
+
+            div.style.color = "red";
+            div.style.border = "1px solid green";
+            div.style.padding = "4px";
+            appDiv = document.querySelector(".App");
+            appDiv.appendChild(div);
+            }
+        
+        function add(a, b) {
+            return a + b;
+        }
+        // Export the functions
+        window.f = {
+            greet: greet,
+            add: add,
+            addDiv: addDiv
+        };
+    '''
+    return Response(js_code, mimetype='text/javascript')
+
 if __name__ == '__main__':
     app.run(debug = True)
